cb30e970f2a6580f6ea439c4da661ebd55fddc5c
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2023-03-19 00:07:09 +0600

    Add files via upload
    The Program will check whether a number is Positive or Negative.

---
+++ Positive_Negative.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n >= 0)
+    {
+        cout << "Positive Number";
+    }
+    else{
+        cout << "Negative Number";
+    }
+    
+    return 0;
+}
